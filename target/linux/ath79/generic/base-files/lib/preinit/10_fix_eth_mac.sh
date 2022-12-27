bc880e335fc6c5b485fd57d8e88a02e4d1ca1de2
Michael Pratt <mcpratt@pm.me>
2022-12-27 01:40:34 -0500

    ath79: add support for Fortinet FAP-221-B (preinit mac and simplify wifi mac)
    Signed-off-by: Michael Pratt <mcpratt@pm.me>

--- target/linux/ath79/generic/base-files/lib/preinit/10_fix_eth_mac.sh
+++ target/linux/ath79/generic/base-files/lib/preinit/10_fix_eth_mac.sh
@@ -19,6 +19,10 @@ preinit_set_mac_address() {
 	siemens,ws-ap3610)
 		ip link set dev eth0 address $(mtd_get_mac_ascii cfg1 ethaddr)
 		;;
+	fortinet,fap-221-b)
+		lan_mac=$(mtd_get_mac_text u-boot 0x3ff80 12)
+		ip link set dev eth0 address $lan_mac
+		;;
 	tplink,deco-s4-v2)
 		base_mac=$(mtd_get_mac_encrypted_deco $(find_mtd_part config))
 		ip link set dev eth0 address $base_mac
