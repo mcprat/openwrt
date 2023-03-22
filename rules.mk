b1f3438e483c2f6f87bc1a99bed7e021d8d514be
Michael Pratt <mcpratt@pm.me>
2023-03-22 18:29:24 -0400

    tools: add gnulib source
    Signed-off-by: Michael Pratt <mcpratt@pm.me>

--- rules.mk
+++ rules.mk
@@ -349,6 +349,7 @@ ifeq ($(CONFIG_BUILD_LOG),y)
   BUILD_LOG:=1
 endif
 
+export HOST_GNULIB_SRCDIR:=$(STAGING_DIR_HOST)/share/gnulib
 export BISON_PKGDATADIR:=$(STAGING_DIR_HOST)/share/bison
 export M4:=$(STAGING_DIR_HOST)/bin/m4
 
