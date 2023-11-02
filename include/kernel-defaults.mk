5af95708713ad9f8b75b29207c816300a0576c78
Michael Pratt <mcpratt@pm.me>
2023-11-02 18:05:16 -0400

    build: use host gzip for decompressing kernel source
    libdeflate-gzip is not recommended for production use by it's authors,
    and the reasons can be seen when trying to decompress a massive tarball,
    like from a kernel snapshot (~ 200 MB), especially on a system with limited memory.
    It is possible to get the following error:
    
    tar: This does not look like a tar archive
    tar: Exiting with failure status due to previous errors
    
    The libdeflate version is perfectly fine for other uses,
    but let's use the host system's gzip for extra large files.
    
    Signed-off-by: Michael Pratt <mcpratt@pm.me>

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -21,7 +21,7 @@ Kernel/Patch:=$(Kernel/Patch/Default)
 ifneq (,$(findstring .xz,$(LINUX_SOURCE)))
   LINUX_CAT:=xzcat
 else
-  LINUX_CAT:=$(STAGING_DIR_HOST)/bin/libdeflate-gzip -dc
+  LINUX_CAT:=$(STAGING_DIR_HOST)/bin/gzip -dc
 endif
 
 ifeq ($(strip $(CONFIG_EXTERNAL_KERNEL_TREE)),"")
