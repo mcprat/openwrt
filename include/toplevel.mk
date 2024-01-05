mcprat/openwrt

commit e4a43cda0efba562d0ed1973b584c21309040ccc

build: allow var.% targets to bypass the prepare steps
These targets are used to input variable values from the Make
context into other things like python scripts, so log messages
should be silenced and build prerequisites should be skipped.

The same thing is done for the other variable print target "val.%".

While at it, combine identical target rules into one definition.

Signed-off-by: Michael Pratt <mcpratt@pm.me>

include/toplevel.mk
@@ -210,9 +210,7 @@ prereq:: prepare-tmpinfo .config
 	@+$(NO_TRACE_MAKE) -r -s $@
 
 check: .config FORCE
-	@+$(NO_TRACE_MAKE) -r -s $@ QUIET= V=s
-
-val.%: FORCE
+check val.% var.%: FORCE
 	@+$(NO_TRACE_MAKE) -r -s $@ QUIET= V=s
 
 WARN_PARALLEL_ERROR = $(if $(BUILD_LOG),,$(and $(filter -j,$(MAKEFLAGS)),$(findstring s,$(OPENWRT_VERBOSE))))

commit 0c0b016a45ffb528686670ffd7a3b6df510eed05

build: fix mixed implicit and normal rules warning
This simplification appeared to be fine, but modern Make doesn't like it.
The warning is "mixed implicit and normal rules: deprecated syntax".

Fixes: e4a43cda0 ("build: allow var.% targets to bypass the prepare steps")
Signed-off-by: Michael Pratt <mcpratt@pm.me>

include/toplevel.mk
@@ -210,7 +210,9 @@ prereq:: prepare-tmpinfo .config
 	@+$(NO_TRACE_MAKE) -r -s $@
 
 check: .config FORCE
-check val.% var.%: FORCE
+	@+$(NO_TRACE_MAKE) -r -s $@ QUIET= V=s
+
+val.% var.%: FORCE
 	@+$(NO_TRACE_MAKE) -r -s $@ QUIET= V=s
 
 WARN_PARALLEL_ERROR = $(if $(BUILD_LOG),,$(and $(filter -j,$(MAKEFLAGS)),$(findstring s,$(OPENWRT_VERBOSE))))